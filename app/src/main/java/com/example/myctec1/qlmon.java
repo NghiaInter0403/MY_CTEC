23fe1dbcd0cdab9df92728889cd7cc020350f2fb
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2026-02-11 11:00:03 +0700

    hoàn thành gán sự kiện các phần tử

--- app/src/main/java/com/example/myctec1/qlmon.java
+++ app/src/main/java/com/example/myctec1/qlmon.java
@@ -1,7 +1,10 @@
 package com.example.myctec1;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class qlmon extends AppCompatActivity {
 EditText edt_mamon,edt_tenmon, edt_tin,edt_nam;
+Spinner cbb_monkhoa;
+Button btn_moi1, btn_luu1, btn_sua1, btn_xoa1, btn_quaylai;
+ListView lv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +27,17 @@ EditText edt_mamon,edt_tenmon, edt_tin,edt_nam;
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // gán sự kiện
+        edt_mamon = findViewById(R.id.edt_mamon);
+        edt_tenmon = findViewById(R.id.edt_tenmon);
+        edt_tin = findViewById(R.id.edt_tin);
+        edt_nam = findViewById(R.id.edt_nam);
+        cbb_monkhoa = findViewById(R.id.cbb_monkhoa);
+        lv1 = findViewById(R.id.lv1);
+        btn_moi1 = findViewById(R.id.btn_moi1);
+        btn_luu1 = findViewById(R.id.btn_luu1);
+        btn_sua1 = findViewById(R.id.btn_sua1);
+        btn_xoa1 = findViewById(R.id.btn_xoa1);
+        btn_quaylai = findViewById(R.id.btn_quaylai);
     }
 }
