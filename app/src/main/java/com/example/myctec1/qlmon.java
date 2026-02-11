cf58a5f224e2fe895cadd5d55e59aea002cdf464
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2026-02-11 11:24:58 +0700

    chỉnh sửa giao diện và thêm chức năng nút mới

--- app/src/main/java/com/example/myctec1/qlmon.java
+++ app/src/main/java/com/example/myctec1/qlmon.java
@@ -1,6 +1,10 @@
 package com.example.myctec1;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -12,11 +16,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class qlmon extends AppCompatActivity {
 EditText edt_mamon,edt_tenmon, edt_tin,edt_nam;
 Spinner cbb_monkhoa;
 Button btn_moi1, btn_luu1, btn_sua1, btn_xoa1, btn_quaylai;
 ListView lv1;
+    ArrayList<String> dsMon = new ArrayList<>();
+    ArrayAdapter<String> adapterMon;
+SQLiteDatabase mydata;
+    private void loadSinhVien() {
+        dsMon.clear();
+
+        Cursor c = mydata.rawQuery(
+                "SELECT mamon,tenmon,sotinchi,namhoc,makhoa FROM monhoc",
+                null
+        );
+
+        while (c.moveToNext()) {
+            String dong =
+                    c.getString(0) + " - " +
+                            c.getString(1) + " - " +
+                            c.getString(2) + " - " +
+                            c.getString(3) + " - " +
+                            c.getString(4);
+            dsMon.add(dong);
+        }
+
+        c.close();
+        adapterMon.notifyDataSetChanged();
+    }
+    // -----------
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,5 +71,23 @@ ListView lv1;
         btn_sua1 = findViewById(R.id.btn_sua1);
         btn_xoa1 = findViewById(R.id.btn_xoa1);
         btn_quaylai = findViewById(R.id.btn_quaylai);
+        adapterMon = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                dsMon
+        );
+        lv1.setAdapter(adapterMon);
+        mydata=openOrCreateDatabase("qlsv1.db",MODE_PRIVATE,null);
+// nút mới
+        btn_moi1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edt_mamon.setText("");
+                edt_tenmon.setText("");
+                edt_tin.setText("");
+                edt_nam.setText("");
+                edt_mamon.requestFocus();
+            }
+        });
     }
 }
