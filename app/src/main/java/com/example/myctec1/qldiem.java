91b3ad736269d333bdca9673e7257021d8918388
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2026-02-14 11:55:23 +0700

    thêm các hàm load cbb cho các phần tử spiner

--- app/src/main/java/com/example/myctec1/qldiem.java
+++ app/src/main/java/com/example/myctec1/qldiem.java
@@ -1,6 +1,11 @@
 package com.example.myctec1;
 
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,6 +14,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -34,6 +40,123 @@ ListView lv2;
     ArrayAdapter<String> adapterNam;
     ArrayList<String> dsDiem = new ArrayList<>();
     ArrayAdapter<String> adapterDiem;
+    SQLiteDatabase mydata;
+    // hàm load
+    // load khoa
+    private ArrayList<String> maKhoaList = new ArrayList<>();
+
+    private void loadKhoa() {
+        dsKhoa.clear();
+        maKhoaList.clear();
+
+        Cursor c = mydata.rawQuery("SELECT makhoa, tenkhoa FROM khoa", null);
+
+        while (c.moveToNext()) {
+            maKhoaList.add(c.getString(0));
+            dsKhoa.add(c.getString(1)); // hiển thị tên khoa
+        }
+        c.close();
+
+        adapterKhoa = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, dsKhoa);
+        adapterKhoa.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        cbb_dkhoa.setAdapter(adapterKhoa);
+    }
+    //load lop
+    private ArrayList<String> maLopList = new ArrayList<>();
+
+    private void loadLop(String makhoa) {
+        dsLop.clear();
+        maLopList.clear();
+
+        Cursor c = mydata.rawQuery(
+                "SELECT malop, tenlop FROM lop WHERE makhoa = ?",
+                new String[]{makhoa});
+
+        while (c.moveToNext()) {
+            maLopList.add(c.getString(0));
+            dsLop.add(c.getString(1));
+        }
+        c.close();
+
+        adapterLop = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, dsLop);
+        adapterLop.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        cbb_dlop.setAdapter(adapterLop);
+    }
+// load sv
+private ArrayList<String> maSvList = new ArrayList<>();
+
+    private void loadSinhVien(String malop) {
+        dsSv.clear();
+        maSvList.clear();
+
+        Cursor c = mydata.rawQuery(
+                "SELECT masv, tensv FROM sinhvien WHERE malop = ?",
+                new String[]{malop});
+
+        while (c.moveToNext()) {
+            maSvList.add(c.getString(0));
+            dsSv.add(c.getString(1));
+        }
+        c.close();
+
+        adapterSv = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, dsSv);
+        adapterSv.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        cbb_dsv.setAdapter(adapterSv);
+    }
+// load mon
+private ArrayList<String> maMonList = new ArrayList<>();
+
+    private void loadMon(String makhoa) {
+        dsMon.clear();
+        maMonList.clear();
+
+        Cursor c = mydata.rawQuery(
+                "SELECT mamon, tenmon FROM monhoc WHERE makhoa = ?",
+                new String[]{makhoa});
+
+        while (c.moveToNext()) {
+            maMonList.add(c.getString(0));
+            dsMon.add(c.getString(1));
+        }
+        c.close();
+
+        adapterMon = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, dsMon);
+        adapterMon.setDropDownViewResource(
+                android.R.layout.simple_spinner_dropdown_item);
+
+        cbb_dmon.setAdapter(adapterMon);
+    }
+// load nam
+private void loadNam(String mamon) {
+    dsNam.clear();
+
+    Cursor c = mydata.rawQuery(
+            "SELECT DISTINCT namhoc FROM monhoc WHERE mamon = ?",
+            new String[]{mamon});
+
+    while (c.moveToNext()) {
+        dsNam.add(c.getString(0));
+    }
+    c.close();
+
+    adapterNam = new ArrayAdapter<>(this,
+            android.R.layout.simple_spinner_item, dsNam);
+    adapterNam.setDropDownViewResource(
+            android.R.layout.simple_spinner_dropdown_item);
+
+    cbb_dnam.setAdapter(adapterNam);
+}
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,5 +183,71 @@ ListView lv2;
         btn_dxoa = findViewById(R.id.btn_dxoa);
         btn_dtrove = findViewById(R.id.btn_dtrove);
         lv2 = findViewById(R.id.lv2);
+        mydata=openOrCreateDatabase("qlsv1.db",MODE_PRIVATE,null);
+        loadKhoa();
+
+        cbb_dkhoa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String makhoa = maKhoaList.get(position);
+
+                loadLop(makhoa);
+                loadMon(makhoa);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        cbb_dlop.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String malop = maLopList.get(position);
+                loadSinhVien(malop);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        cbb_dmon.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                String mamon = maMonList.get(position);
+                loadNam(mamon);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+// set các nút
+        // nút trở về
+        btn_dtrove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(qldiem.this);
+                builder.setTitle("QUAY VỀ ?");
+                builder.setMessage("Bạn có muốn quay về trang chủ không?");
+
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish(); // thoát chương trình
+                    }
+                });
+
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); // quay lại chương trình
+                    }
+                });
+
+                builder.show();
+            }
+        });
     }
 }
