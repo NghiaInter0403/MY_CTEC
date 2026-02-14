a848afe4a25a36842591dd0ce4d97d618942949f
NghiaInter0403 <phanhieunghia13052019@gmail.com>
2026-02-14 11:23:55 +0700

    ánh xạ các phần tử

--- app/src/main/java/com/example/myctec1/qldiem.java
+++ app/src/main/java/com/example/myctec1/qldiem.java
@@ -1,6 +1,11 @@
 package com.example.myctec1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class qldiem extends AppCompatActivity {
-
+TextView tv_dtb;
+EditText edt_diem1,edt_diem2,edt_diem3;
+Spinner cbb_dkhoa, cbb_dlop, cbb_dsv, cbb_dmon, cbb_dnam;
+Button btn_dmoi, btn_dluu, btn_dsua, btn_dxoa, btn_dtrove;
+ListView lv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,20 @@ public class qldiem extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // ánh xạ
+        tv_dtb = findViewById(R.id.tv_dtb);
+        edt_diem1 = findViewById(R.id.edt_diem1);
+        edt_diem2 = findViewById(R.id.edt_diem2);
+        edt_diem3 = findViewById(R.id.edt_diem3);
+        cbb_dkhoa = findViewById(R.id.cbb_dkhoa);
+        cbb_dlop = findViewById(R.id.cbb_dlop);
+        cbb_dsv = findViewById(R.id.cbb_dsv);
+        cbb_dmon = findViewById(R.id.cbb_dmon);
+        cbb_dnam = findViewById(R.id.cbb_dnam);
+        btn_dmoi = findViewById(R.id.btn_dmoi);
+        btn_dluu = findViewById(R.id.btn_dluu);
+        btn_dsua = findViewById(R.id.btn_dsua);
+        btn_dxoa = findViewById(R.id.btn_dxoa);
+        btn_dtrove = findViewById(R.id.btn_dtrove);
     }
 }
